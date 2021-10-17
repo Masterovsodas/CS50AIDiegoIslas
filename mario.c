c4b87bd39a0e562b25f686c4fd3c4909c570a8cd
Masterovsodas <Masterovsodas@users.noreply.github.com>
2021-10-17 20:11:02 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -6,45 +6,41 @@ int main(void)
 {
     //essential vars
     int height = 0;
-    
+
     //verify validity
     do
     {
         height = get_int("Height: ");
     }
-    while(height <= 0 || height > 8);
+    while (height <= 0 || height > 8);
 
     ///build pyramids
-    for(int i = 1; i <= height; i++)
+    for (int i = 1; i <= height; i++)
     {
-        //okay so cs50.h has  built in string which does jack, so we ave to use c strings, very stupid char arrays
-        char side1[10];
-        char side2[10];
         int spaces = height - i;
-        
+
         //get necessary characters based on data we have; until i > spaces, print only spaces, after get #; this will give the right side of the pyramid always.
-        for(int j = 0; j<height; j++){
+        for (int j = 0; j < height; j++)
+        {
             //when less add a space
-            if(j < spaces)
+            if (j < spaces)
             {
-               side1[j] = ' ';
+               printf(" ");
             }
             //when more add tags
             else
             {
-                side1[j] = '#';
+                printf("#");
             }
         }
-       // printf("side1: %s\n", side1);
-        //reverse string and make side2 euqal that
-       for(int j = 0; j < height; j++)
-       {
-           if(side1[(height - 1) - j] == '#')
-           {
-               side2[j] = side1[(height - 1) - j];
-           }
-       }
         
-        printf("%s  %s\n", side1, side2);
+        //print gap
+        printf("  ");
+        //reverse string and make side2 euqal that
+        for (int j = 0; j < i; j++)
+        {
+         printf("#");
+        }
+        printf("\n");
     }
 }
