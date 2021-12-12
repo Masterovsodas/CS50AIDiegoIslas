bdaa613e5c1d19188a9aa9acefaad6bbf25d44bf
Masterovsodas <Masterovsodas@users.noreply.github.com>
2021-12-12 22:30:54 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -143,7 +143,7 @@ bool unload(void)
             node *temp = cursorList;
             free(cursorList);
             cursorList = temp->next;
-            //free(temp);
+            free(temp);
         }
     }
     return true;
